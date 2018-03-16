23b182fbdb2057316c4780a416c51de0f3c593c8
unknown <flavien.baillargeau@gmail.com>
2018-03-16 16:52:09 +0100

    Ajout de variables d'environnements

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,6 @@
 module.exports = {
     NODE_ENV: '"production"',
     HTTP_GOOGLE_SCRIPT: '"https://script.google.com/a/palo-it.com/macros/s/AKfycbz8_ZfTsW_mq7vdpGrWuwSyEWuVjOuM3hWYwXx6iQO8cS6cRZoq/exec"',
-    FITNET_AUTHORIZATION: '"ZmJhaWxsYXJnZWF1QHBhbG8taXQuY29tOnZoVDczOSFIRmU="'
+    FITNET_AUTHORIZATION: '"ZmJhaWxsYXJnZWF1QHBhbG8taXQuY29tOnZoVDczOSFIRmU="',
+    FITNET_ENDPOINT: '"https://evaluation.fitnetmanager.com"'
 }
--- src/services/mission.service.js
+++ src/services/mission.service.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default {
     getMissions: function() {
-        return axios.get('api/FitnetManager/rest/contracts/1', {
+        return axios.get(process.env.FITNET_ENDPOINT + '/FitnetManager/rest/contracts/1', {
                 headers: {
                     "Content-Type": "application/json; charset=utf-8",
                     "Authorization": "Basic " + process.env.FITNET_AUTHORIZATION
@@ -16,7 +16,7 @@ export default {
             })
     },
     getMission: function(id) {
-        return axios.get(`api/FitnetManager/rest/contracts/read/${id}`, {
+        return axios.get(process.env.FITNET_ENDPOINT + `/FitnetManager/rest/contracts/read/${id}`, {
                 headers: {
                     "Content-Type": "application/json; charset=utf-8",
                     "Authorization": "Basic " + process.env.FITNET_AUTHORIZATION
